5267fc391e742eff5b9b0494ab9e3e6b0b8f7a58
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-11-03 23:54:08 +0100

    Interesting

--- main.cpp
+++ main.cpp
@@ -1,15 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define GC_INCLUDE_NEW
 #define GC_DEBUG
 #include <gc/gc.h>
 #include <gc/gc_cpp.h>
 #include <gc/gc_allocator.h>
 
+struct LoL {
+    int test;
+};
+
 int main(int argc, char** argv) {
-    char *array = new char[100];
-    sprintf(array, "%d", argc);
-    printf("%s",array);
+    LoL *array = new LoL[100];
+    printf("%d",array[0].test);
+
+#if defined(__has_feature)
+# if __has_feature(address_sanitizer) && !defined(ADDRESS_SANITIZER)
+    printf ("We has ASAN!\n");
+# else
+    printf ("We have has_feature, no ASAN!\n");
+# endif
+#else
+    printf ("We got nothing!\n");
+#endif
 
     GC_gcollect();
 
