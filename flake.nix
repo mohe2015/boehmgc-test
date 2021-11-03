3acc268cb536d85e25bea015f5139d81e2638707
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-11-03 23:18:48 +0100

    This works

--- flake.nix
+++ flake.nix
@@ -10,10 +10,11 @@
         defaultPackage."x86_64-linux" = pkgs.llvmPackages_13.stdenv.mkDerivation {
             name = "test";
             src = ./.;
+            nativeBuildInputs = [ pkgs.llvm_13 ];
             buildInputs = [ pkgs.boehmgc ];
             buildPhase = ''
                 clang++ -Wall -Wextra -fno-omit-frame-pointer -g -O1 -fsanitize=address -c main.cpp
-                clang++ -Wall -Wextra -fno-omit-frame-pointer -g -O1 -fsanitize=address -o test main.o
+                clang++ -Wall -Wextra -fno-omit-frame-pointer -g -O1 -fsanitize=address -lgc -lgccpp -o test main.o
             '';
             installPhase = ''
                 cp test $out
--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-#define GC_INCLUDE_NEW
 #define GC_DEBUG
 #include <gc/gc.h>
 #include <gc/gc_cpp.h>
 #include <gc/gc_allocator.h>
-*/
 
 int main(int argc, char** argv) {
-    char * buffer = (char*)malloc(1024);
-    sprintf(buffer, "%d", argc);
-    printf("%s",buffer);
+    char *array = new char[100];
+    sprintf(array, "%d", argc);
+    printf("%s",array);
+
+    GC_gcollect();
 
     return 0;
 }
