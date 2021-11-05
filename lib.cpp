a9b504f6860d384484c05f3870b305bd608c488e
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-11-05 01:28:22 +0100

    obv this leaks because struct is not gc

--- lib.cpp
+++ lib.cpp
@@ -9,67 +9,26 @@
 #include <gc/gc_cpp.h>
 #include <gc/gc_allocator.h>
 
-
-// I think this is a false flag thing from the place where they fake-copy them
-struct RegisterPrimOp : gc
+struct Info
 {
-    struct Info
-    {
-        std::string name;
-        std::vector<std::string> args;
-        size_t arity = 0;
-        const char * doc;
-    };
-
-    typedef std::vector<Info> PrimOps;
-    static PrimOps * primOps;
-
-    RegisterPrimOp(Info info);
+    std::string name;
+    std::vector<std::string> args;
+    size_t arity = 0;
+    const char * doc;
 };
-
-struct Value : gc
-{
-public:
-    
-    RegisterPrimOp::Info * primOp;
-
-    inline void mkPrimOp(RegisterPrimOp::Info* i)
-    {
-        primOp = i;
-    }
-};
-
-RegisterPrimOp::PrimOps * RegisterPrimOp::primOps;
-
-RegisterPrimOp::RegisterPrimOp(Info info)
-{
-    if (!primOps) primOps = new PrimOps;
-    primOps->push_back(std::move(info));
-}
-
-static RegisterPrimOp test2({
-    .name = "__pathExists",
-    .args = {"path","flsdfhdsifsf","hdsflisdhfdsif","dslfihsfldfhs"},
-    .doc = R"(
-      Return `true` if the path *path* exists at evaluation time, and
-      `false` otherwise.
-    )",
-});
 
 int libtest() {
     //int *array = new int[100];
     //printf("%d",array[0]);
 
-    Value v;
-
-    v.mkPrimOp(new RegisterPrimOp::Info({
+    new Info({
         .name = "__pathExists",
         .args = {"path","flsdfhdsifsf","hdsflisdhfdsif","dslfihsfldfhs"},
         .doc = R"(
         Return `true` if the path *path* exists at evaluation time, and
         `false` otherwise.
         )",
-    }));
+    });
 
     return 7;
 }
